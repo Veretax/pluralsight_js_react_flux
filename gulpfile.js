de50634cec89a8a8050abc475125fdd20e401ae1
Tim Western <veretax@gmail.com>
2018-06-29 06:54:17 -0400

    Browserify, reactify, setup Index and main.js setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local dev server
 var open = require('gulp-open'); // Open a URL in a web browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JS to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: '.src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 // Start a local dev server
@@ -36,9 +41,20 @@ gulp.task('html', function () {
         .pipe(connect.reload())
 });
 
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.html, ['js']);
 });
 
 // Default task
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
