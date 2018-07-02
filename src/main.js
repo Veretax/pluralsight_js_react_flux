21dcb7df177ac45688573bd3cd723a22fd1cea38
Tim Western <veretax@gmail.com>
2018-07-02 09:13:23 -0400

    add use strict to main.js still not rendering.

--- src/main.js
+++ src/main.js
@@ -6,24 +6,28 @@ var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
 var App = React.createClass({
-    render: function () {
-        var Child;
-        
-        switch(this.props.route) {
-            case 'about': Child = About; break;
-            default: Child = Home;
-        }
-        return (
-            <div>
-                <Child/>
-            </div>
-        );
+  render: function() {
+    var Child;
+
+    switch (this.props.route) {
+      case 'about':
+        Child = About;
+        break;
+      default:
+        Child = Home;
     }
+
+    return (
+      <div>
+        <Child />
+      </div>
+    );
+  }
 });
 
 function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
+  var route = window.location.hash.substr(1);
+  React.render(<app route={route} />, document.getElementById('app'));
 }
 
 window.addEventListener('hashchange', render);
